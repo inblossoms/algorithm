69c0baa4073ff2d2cde2a79dc471f37762aa8abf
MEIYA-yl <2648297163@qq.com>
2022-07-04 22:05:21 +0800

    算法题：期股收益 2

--- 算法题/期股最大收益.js
+++ 算法题/期股最大收益.js
@@ -22,3 +22,20 @@ function maxmumReturn(prices) {
 let price = [2, 4, 6, 3, 7, 1, 9, 5];
 maxmumReturn(price);
 console.log(maxmumReturn(price));
+
+// Tip 在上一次的基础上，你可以多次购买不同时期的股票，但在购买下一次的股票之前必须抛售之前的股票
+
+// 思路：在每一次的价格低谷收入，在下一次的价格相对于上次有利可图抛出
+
+function maxmumReturn(prices) {
+  if (prices == null || prices.length === 0) return 0;
+
+  let ans = 0;
+  for (let i = 1; i < prices.length; i++) {
+    ans += Math.max(prices[i] - prices[i - 1], 0);
+  }
+
+  return ans;
+}
+
+// Tip 在上一次的条件上，限制交易次数为两次，保证最大收益
