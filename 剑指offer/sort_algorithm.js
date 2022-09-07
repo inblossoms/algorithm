1833ea8db354c5136eade3287eff09304fcee8f8
MEIYA-yl <2648297163@qq.com>
2022-09-07 16:23:14 +0800

    chore(sort_algorithm.js): quickSort
    added quickSort method
    
    [skip ci]

--- 剑指offer/sort_algorithm.js
+++ 剑指offer/sort_algorithm.js
@@ -1,6 +1,6 @@
 import {less, swapTowValue} from './tools.js';
 
-let arr = [1, 33, 43, 12, 2, 21, 324]
+let arr = [1, 33, 43, 12, 2, 21, 324, 0, 12];
 
 /**
  * 冒泡排序：比较相邻的两个元素，如果第一个比第二个大，就交换他们两个。
@@ -12,21 +12,20 @@ let arr = [1, 33, 43, 12, 2, 21, 324]
  * 稳定性：稳定
  */
 function bubbleSort(arr) {
-  if (arr == null || arr.length <= 1) return
+  if (arr == null || arr.length <= 1) return;
 
-  const len = arr.length
+  const len = arr.length;
   for (let i = 0; i < len - 1; i++) {
     for (let j = 0; j < len - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
-        swapTowValue(arr, j, j + 1)
+        swapTowValue(arr, j, j + 1);
       }
     }
   }
-  return arr
+  return arr;
 }
 
-console.log('bubbleSort:', bubbleSort(arr))
-
+console.log('bubbleSort:', bubbleSort(arr));
 
 /**
  * 选择排序：将序列中最大（小）的值排序到起始位置，然后将未排序的值排序到已排序序列的末尾，重复该操作。
@@ -39,20 +38,20 @@ console.log('bubbleSort:', bubbleSort(arr))
  * 稳定性：不稳定
  */
 const selectionSort = (arr) => {
-  if (arr == null || arr.length <= 1) return
+  if (arr == null || arr.length <= 1) return;
 
-  const len = arr.length
+  const len = arr.length;
   for (let i = 0; i < len - 1; i++) {
-    let idx = i
+    let idx = i;
     for (let j = i + 1; j < len; j++) {
-      if (arr[idx] > arr[j]) idx = j          // 找到从当前位置之后剩余数值中最小值的索引
+      if (arr[idx] > arr[j]) idx = j; // 找到从当前位置之后剩余数值中最小值的索引
     }
-    swapTowValue(arr, i, idx)
+    swapTowValue(arr, i, idx);
   }
-  return arr
-}
+  return arr;
+};
 
-console.log('selectionSort:', selectionSort(arr))
+console.log('selectionSort:', selectionSort(arr));
 
 /**
  * 插入排序：从当前值开始，看左侧值是否大于当前值，循环直到当前值大于左侧值位置。
@@ -64,23 +63,23 @@ console.log('selectionSort:', selectionSort(arr))
  * 稳定性：稳定
  */
 const insertSort = (arr) => {
-  if (arr == null || arr.length <= 1) return
+  if (arr == null || arr.length <= 1) return;
 
-  const len = arr.length
-  let preIdx, current
+  const len = arr.length;
+  let preIdx, current;
   for (let i = 1; i < len; i++) {
-    preIdx = i - 1
-    current = arr[i]
+    preIdx = i - 1;
+    current = arr[i];
     while (preIdx > 0 && current < arr[preIdx]) {
-      arr[preIdx + 1] = arr[preIdx]       // 只要当前值小于上一个值 就用当前值将上一个值替换
-      preIdx--               // 移动指针位置 保证当前循环所比较的值的正确性
+      arr[preIdx + 1] = arr[preIdx]; // 只要当前值小于上一个值 就用当前值将上一个值替换
+      preIdx--; // 移动指针位置 保证当前循环所比较的值的正确性
     }
-    arr[preIdx + 1] = current
+    arr[preIdx + 1] = current;
   }
-  return arr
-}
+  return arr;
+};
 
-console.log('insertSort:', insertSort(arr))
+console.log('insertSort:', insertSort(arr));
 
 /**
  * 希尔排序：先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序，待整个序列中的记录"基本有序"时，
@@ -94,24 +93,27 @@ console.log('insertSort:', insertSort(arr))
  */
 
 const shellSort = (arr) => {
-  if (arr == null || arr.length <= 1) return
+  if (arr == null || arr.length <= 1) return;
 
   let N = arr.length,
     gap = 1;
   while (gap < N / 3) {
-    gap = gap * 3 + 1
-  }         // 动态定义间隔的增量序列 （算法4给出的初始逻辑）
-  for (gap; gap > 0; gap = Math.floor(gap / 3)) {       // 处于性能考虑的写法：可以判断gap >= 1  gap / 2 也可
-    for (let i = gap; i < N; i++) {         // 表示：每一次跳跃一个当前gap，然后依次为后续参数做插入
-      for (let j = i; j >= gap && less(arr, j, j - gap); j -= gap) {        // 这里就是插入排序
-        swapTowValue(arr, j, j - gap)
+    gap = gap * 3 + 1;
+  } // 动态定义间隔的增量序列 （算法4给出的初始逻辑）
+  for (gap; gap > 0; gap = Math.floor(gap / 3)) {
+    // 处于性能考虑的写法：可以判断gap >= 1  gap / 2 也可
+    for (let i = gap; i < N; i++) {
+      // 表示：每一次跳跃一个当前gap，然后依次为后续参数做插入
+      for (let j = i; j >= gap && less(arr, j, j - gap); j -= gap) {
+        // 这里就是插入排序
+        swapTowValue(arr, j, j - gap);
       }
     }
   }
-  return arr
-}
+  return arr;
+};
 
-console.log('shellSort:', shellSort(arr))
+console.log('shellSort:', shellSort(arr));
 
 /**
  * 归并排序：先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序，待整个序列中的记录"基本有序"时，
@@ -125,34 +127,62 @@ console.log('shellSort:', shellSort(arr))
  */
 
 const mergeSort = (arr) => {
-  let len = arr.length
-  if (len < 2) return arr
+  let len = arr.length;
+  if (len < 2) return arr;
 
-//  将数组进行拆分
+  //  将数组进行拆分
   let mid = Math.floor(len >> 1),
     leftAry = arr.slice(0, mid),
-    rightAry = arr.slice(mid)
-  return merge(mergeSort(leftAry), mergeSort(rightAry))     // 每一次的拆分都进行一次合并
-
+    rightAry = arr.slice(mid);
+  return merge(mergeSort(leftAry), mergeSort(rightAry)); // 每一次的拆分都进行一次合并
+  //  合并
   function merge(l, r) {
-    let ary = []
-    while (l.length && r.length) l[0] < r[0] ? ary.push(l.shift()) : ary.push(r.shift())
-    while (l.length) ary.push(l.shift())
-    while (r.length) ary.push(r.shift())
+    let ary = [];
+    while (l.length && r.length)
+      l[0] < r[0] ? ary.push(l.shift()) : ary.push(r.shift()); // 将树形的左右两分支的元素进行比对合并处理
+    while (l.length) ary.push(l.shift());
+    while (r.length) ary.push(r.shift());
 
-    return ary
+    return ary;
   }
-}
+};
 
-console.log('mergeSort:', mergeSort(arr))
+console.log('mergeSort:', mergeSort(arr));
 
+/**
+ * 快速排序：先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序，待整个序列中的记录"基本有序"时，
+ *         再对全体记录进行依次直接插入排序。
+ * 平均时间复杂度：O(n log n)
+ * 最好情况：O(n log n)
+ * 最坏情况：O(n^2)
+ * 空间复杂度：O(log n)
+ * 排序方式：in-place
+ * 稳定性：不稳定
+ */
 
+const quickSort = (arr) => {
+  //  递归的结束条件
 
+  if (arr.length <= 1) {
+    return arr;
+  } // 长度为一的数组不需要排序
+  //  1.  设置基准元素 ->　pivot，pivot可以是数组元素中的任意一个
+  let center = Math.floor(arr.length >> 1),
+    pivot = arr.splice(center, 1)[0],
+    leftAry = [],
+    rightAry = [];
 
+  //  2.  对元素进行排列，分别将大小于pivot的值排列在其两端
+  for (let i = 0; i < arr.length; i++) {
+    let target = arr[i];
+    if (target < pivot) {
+      leftAry.push(target);
+    } else {
+      rightAry.push(target);
+    }
+  }
+  //  3.  递归调用
+  return [...quickSort(leftAry), pivot, ...quickSort(rightAry)];
+};
 
-
-
-
-
-
-
+console.log('quickSort：', quickSort(arr));
