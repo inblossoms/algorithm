741125e054f0864ce8d07bd0c681041bb2dc6e02
MEIYA-yl <2648297163@qq.com>
2022-09-19 11:51:59 +0800

    chore(数组): added description
    [skip ci]

--- 编程题/数组/删除元素.js
+++ 编程题/数组/删除元素.js
@@ -1,4 +1,4 @@
-// 通过双指针进行移除操作
+// 通过双指针进行移除操作：删除数组中指定的目标元素，并返回目标值在数组中的索引位置
 
 const removeEle = (nums, target) => {
   let slowIdx = 0, len = nums.length
@@ -11,4 +11,4 @@ const removeEle = (nums, target) => {
 }
 
 let nums = [12, 34, 24, 43, 546, 76, 81, 34, 22, 34]
-console.log(removeEle(nums,546));
+console.log(removeEle(nums, 546));
