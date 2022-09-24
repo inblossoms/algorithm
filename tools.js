390caf0b94b12ff8284266285eeb697754a25fad
MEIYA-yl <2648297163@qq.com>
2022-09-24 10:17:00 +0800

    chore: formating

--- tools.js
+++ tools.js
@@ -1,11 +1,11 @@
 // 交换两数值
 export const swapTowValue = (arr, x, y) => {
-  let temp = arr[x]
-  arr[x] = arr[y]
-  arr[y] = temp
-}
+  let temp = arr[x];
+  arr[x] = arr[y];
+  arr[y] = temp;
+};
 
 // 比较两数值的大小：如若 v1 < v2 则返回 true
 export const less = (arr, v1, v2) => {
-  return arr[v1] < arr[v2]
-}
+  return arr[v1] < arr[v2];
+};
