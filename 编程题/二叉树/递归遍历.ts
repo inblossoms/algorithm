9d76e3c0c034a177f11ac99e1c190175c4fe9c2d
inblossoms <L13994352192@163.com>
2022-10-24 22:30:57 +0800

    chore: 递归遍历 - 前中后序遍历

---
+++ 编程题/二叉树/递归遍历.ts
@@ -0,0 +1,59 @@
+/**
+ * 递归三要素：
+ * 1. 确定递归函数的参数和返回值
+ * 2. 确定终止条件
+ * 3. 确定单层递归的逻辑
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+// 前序排列：中左右
+function preorderTraversal(node: TreeNode | null): number[] {
+  const traverse = (node: TreeNode | null, res: number[]): void => {
+    if (node === null) return;
+
+    res.push(node.val);
+    traverse(node.left, res);
+    traverse(node.right, res);
+  };
+
+  const res: number[] = [];
+  traverse(node, res);
+
+  return res;
+}
+
+// 中序遍历
+function inorderTraversal(node: TreeNode | null): number[] {
+  function traverse(node: TreeNode | null, res: number[]): void {
+    if (node === null) return;
+    traverse(node.left, res);
+    res.push(node.val);
+    traverse(node.right, res);
+  }
+  const res: number[] = [];
+  traverse(node, res);
+  return res;
+}
+
+// 后序遍历
+function postorderTraversal(node: TreeNode | null): number[] {
+  function traverse(node: TreeNode | null, res: number[]): void {
+    if (node === null) return;
+    traverse(node.left, res);
+    traverse(node.right, res);
+    res.push(node.val);
+  }
+  const res: number[] = [];
+  traverse(node, res);
+  return res;
+}
